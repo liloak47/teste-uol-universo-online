8ce3d606b0b46b0e9a066329f8a9db6e0447525e
Murillo Augusto <murilloaugusto@vxcaseloja.com>
2022-07-26 18:17:46 -0300

    feat: exercie one and two of logic done

---
+++ logica/ex2.js
@@ -0,0 +1,18 @@
+const techOpsPrint = (limit, steps) =>{
+    if(steps === 0){
+        console.error("step param incorrect; steps must be more then 0 ")
+    }else{
+        for(let i = 0; i <= limit; i +=steps){
+            if( i%3 ==0 && i%5 == 0){
+                console.log('TechOps');
+            }else if(i%3 ==0){
+                console.log('Tech');
+            }else if(i % 5 == 0){
+                console.log('Ops');
+            }else{
+                console.log(i);
+            }
+        }
+    }
+}
+techOpsPrint(23,0);
